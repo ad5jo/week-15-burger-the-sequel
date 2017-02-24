5ca8a64d056b223a7ce6a920c06bcab091be7cd5
Dave Durkee <ad5jo@yahoo.com>
2017-02-23 18:38:58 -0600

    try to fix jaws database

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Ben Roethlisberger');
-INSERT INTO burgers (burger_name) VALUES ('Luxembourger');
-INSERT INTO burgers (burger_name) VALUES ('Pittsburgher');
